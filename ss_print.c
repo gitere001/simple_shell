d144102b23690e521ad56312e3ed7eb9f7aa837f
gitere001 <sasmuypwa2019@gmail.com>
2023-11-03 18:42:11 +0300

    fuction that prints to the stdout, works the same as printf

---
+++ ss_print.c
@@ -0,0 +1,11 @@
+#include "sshell.h"
+
+/**
+* ss_print - a function that print input to stdout
+* @input: string passed
+* Return: null
+*/
+void ss_print(const char *input)
+{
+	write(STDOUT_FILENO, input, strlen(input));
+}

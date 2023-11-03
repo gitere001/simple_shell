d981e203312b5acf7aa58137f168e5a6124ea8c3
gitere001 <sasmuypwa2019@gmail.com>
2023-11-03 18:44:31 +0300

    A fuction that displays to prompt the user to enter command

---
+++ prompt_dispay.c
@@ -0,0 +1,12 @@
+#include "sshell.h"
+
+/**
+* prompt_display - displays a prompt to show that shell is ready to
+* accept input
+* Return: void
+*/
+void prompt_display(void)
+{
+	ss_print("our shell$ ");
+}
+

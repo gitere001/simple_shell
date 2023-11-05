6c1143f155dad8ca33dce1ab91b0e52ef9316660
gitere001 <sasmuypwa2019@gmail.com>
2023-11-05 18:47:51 +0300

    updated main function to accomodate tokenization function

--- sshell.c
+++ sshell.c
@@ -7,12 +7,14 @@
 int main(void)
 {
 	char user_command[150];
+	const char *args[MAX_ARGS];
 
 	while (1)
 	{
 		prompt_display();
 		read_user_command(user_command, sizeof(user_command));
-		exec_command(user_command);
+		tokenize_command(user_command, args);
+		execute_command(user_command);
 	}
 
 	return (0);

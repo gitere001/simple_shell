8fbc10c32414e46ab866ce8cad3f9e2ee9042fa4
gitere001 <sasmuypwa2019@gmail.com>
2023-11-05 18:46:37 +0300

    edited to handle user arguments

--- command_executor.c
+++ command_executor.c
@@ -1,39 +1,35 @@
 #include "sshell.h"
-
 /**
- * exec_command - execute the command of the child process
- * @user_command: the command that the user input
- * Return: 0 for success
+ * execute_command - Execute a user command in a child process
+ * @user_command: The user-provided command to execute
+ *
+ * This function takes a user command, tokenizes it into arguments, and
+ * executes it in a child process using execve. It waits for the child
+ * process to complete and handles errors.
+ *
+ * @user_command: The command to execute.
  */
-void exec_command(const char *user_command)
+void execute_command(const char *user_command)
 {
+	const char *args[MAX_ARGS];
 	pid_t child_id = fork();
-	char **argv = malloc(2 * sizeof(char *));
 
-	if (argv == NULL)
-	{
-	perror("malloc");
-	exit(EXIT_FAILURE);
-	}
-
-	argv[0] = (char *)user_command;
-	argv[1] = NULL;
+	tokenize_command(user_command, args);
 
 	if (child_id == -1)
 	{
-	perror("fork");
-	exit(EXIT_FAILURE);
+		perror("fork");
+		exit(EXIT_FAILURE);
 	}
 	else if (child_id == 0)
 	{
-	execve(user_command, argv, NULL);
-	perror("execve");
-	exit(EXIT_FAILURE);
+		execve(args[0], (char *const *)args, NULL);
+		perror("execve");
+		exit(EXIT_FAILURE);
 	}
 	else
 	{
 		wait(NULL);
 	}
-
-	free(argv);
 }
+

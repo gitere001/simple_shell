69d59323c6f8903a7a968b94c6a85faa4ac13fd6
gitere001 <sasmuypwa2019@gmail.com>
2023-11-05 18:45:09 +0300

    updated header file

--- sshell.h
+++ sshell.h
@@ -6,9 +6,12 @@
 #include <string.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+
+#define MAX_ARGS 100
+
 void prompt_display(void);
 void ss_print(const char *input);
-void exec_command(const char *user_command);
 void read_user_command(char *user_command, size_t size);
-
+void tokenize_command(const char *user_command, const char *args[]);
+void execute_command(const char *user_command);
 #endif

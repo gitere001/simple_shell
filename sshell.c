140fb514a1949923103366cc6f42f8cc89cd8906
gitere001 <sasmuypwa2019@gmail.com>
2023-11-07 08:00:07 +0300

    updated main function

--- sshell.c
+++ sshell.c
@@ -7,13 +7,15 @@
 int main(void)
 {
 	char user_command[150];
-	const char *args[MAX_ARGS];
 
 	while (1)
 	{
 		prompt_display();
 		read_user_command(user_command, sizeof(user_command));
-		tokenize_command(user_command, args);
+		if (strcmp(user_command, "exit") == 0)
+		{
+			break;
+		}
 		execute_command(user_command);
 	}
 

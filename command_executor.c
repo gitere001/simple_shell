9866b9d6ff6acddda9781b857f644773bb978ceb
gitere001 <sasmuypwa2019@gmail.com>
2023-11-07 07:59:11 +0300

    updated function

--- command_executor.c
+++ command_executor.c
@@ -1,20 +1,26 @@
 #include "sshell.h"
+
 /**
- * execute_command - Execute a user command in a child process
- * @user_command: The user-provided command to execute
- *
- * This function takes a user command, tokenizes it into arguments, and
- * executes it in a child process using execve. It waits for the child
- * process to complete and handles errors.
- *
- * @user_command: The command to execute.
+ * execute_command - Tokenize the user input, find the command path,
+ * and execute it in a child process.
+ * @user_command: The user-provided command to execute.
+ * Return: void
  */
 void execute_command(const char *user_command)
 {
 	const char *args[MAX_ARGS];
 	pid_t child_id = fork();
 
-	tokenize_command(user_command, args);
+	size_t num_args = 0;
+	char *token = strtok(strdup(user_command), " ");
+
+	while (token != NULL && num_args < MAX_ARGS - 1)
+	{
+		args[num_args++] = token;
+		token = strtok(NULL, " ");
+	}
+
+	args[num_args] = NULL;
 
 	if (child_id == -1)
 	{
@@ -23,13 +29,24 @@ void execute_command(const char *user_command)
 	}
 	else if (child_id == 0)
 	{
-		execve(args[0], (char *const *)args, NULL);
-		perror("execve");
-		exit(EXIT_FAILURE);
+		const char *full_path = find_command_path(args[0]);
+
+		if (full_path != NULL)
+		{
+			execve(full_path, (char *const *)args, NULL);
+			perror("execve");
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			ss_print("Command not found: ");
+			ss_print(args[0]);
+			ss_print("\n");
+			exit(EXIT_FAILURE);
+		}
 	}
 	else
 	{
 		wait(NULL);
 	}
 }
-

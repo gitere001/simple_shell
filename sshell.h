bcda82183c5ced47bdc0dc63023785e02f08d990
gitere001 <sasmuypwa2019@gmail.com>
2023-11-07 21:20:02 -0800

    added print_envir prototype

--- sshell.h
+++ sshell.h
@@ -8,10 +8,12 @@
 #include <sys/wait.h>
 
 #define MAX_ARGS 100
+extern char **environ;
 
 void prompt_display(void);
 void ss_print(const char *input);
 void read_user_command(char *user_command, size_t size);
 const char *find_command_path(const char *command);
 void execute_command(const char *user_command);
+void print_environment(void);
 #endif

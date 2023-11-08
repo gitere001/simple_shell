c1608bd417a1b478a6a8072d400e661242703339
gitere001 <sasmuypwa2019@gmail.com>
2023-11-08 11:35:09 +0300

    updated main function

--- sshell.c
+++ sshell.c
@@ -1,30 +1,44 @@
 #include "sshell.h"
 
 /**
-* main - entry point
-* Return: 0 for success
-*/
+ * main - entry point
+ * Return: 0 for success
+ */
 int main(void)
 {
-	char user_command[150];
+	char *user_command = NULL;
+	size_t user_command_size = 0;
 
 	while (1)
 	{
 		prompt_display();
-		read_user_command(user_command, sizeof(user_command));
+		if (getline(&user_command, &user_command_size, stdin) == -1)
+		{
+
+			break;
+		}
+
+		if (user_command[strlen(user_command) - 1] == '\n')
+		{
+			user_command[strlen(user_command) - 1] = '\0';
+		}
+			handle_exit_command(user_command);
+
 		if (strcmp(user_command, "exit") == 0)
 		{
+			free(user_command);
 			break;
 		}
 		else if (strcmp(user_command, "env") == 0)
- 		{
-		print_environment();
- 		}
+		{
+			print_environment();
+		}
 		else
 		{
-		execute_command(user_command);
+			execute_command(user_command);
 		}
 	}
 
+	free(user_command);
 	return (0);
 }

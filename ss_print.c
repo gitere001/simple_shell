e93a0849550cf3392ae85535fd0d808199f5f92c
James gitere <sasmuypwa2019.com>
2023-11-15 12:16:38 -0800

    updated

--- ss_print.c
+++ ss_print.c
@@ -2,10 +2,38 @@
 
 /**
 * ss_print - a function that print input to stdout
-* @input: string passed
+* @str: string passed
 * Return: null
 */
-void ss_print(const char *input)
+void ss_print(char *str)
 {
-	write(STDOUT_FILENO, input, strlen(input));
+	size_t len;
+	ssize_t bytes_written;
+
+	len = _strlen(str);
+	bytes_written = write(STDOUT_FILENO, str, len);
+	if (bytes_written == -1)
+	{
+		perror("write");
+	}
+
+}
+
+/**
+ * _printerror - prints error in standard error
+ * @error: error to be printed
+ * Return: void
+*/
+void _printerror(char *error)
+{
+	size_t len;
+	ssize_t bytes_written;
+
+	len = _strlen(error);
+	bytes_written = write(STDERR_FILENO, error, len);
+	if (bytes_written == -1)
+	{
+		perror("write");
+	}
+
 }

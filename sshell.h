9e5ba135e4428d90bb5a37943fa8806fdf00fd1d
James gitere <sasmuypwa2019.com>
2023-11-15 12:14:12 -0800

    modified header file

--- sshell.h
+++ sshell.h
@@ -2,23 +2,27 @@
 #define SSHELL_H
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <stdlib.h>
+#include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdarg.h>
 #include <ctype.h>
 
-#define MAX_ARGS 100
-extern char **environ;
-
+void ss_print(char *str);
+void _printerror(char *error);
 void prompt_display(void);
-void handle_exit_command(const char *command);
-void ss_print(const char *input);
-void read_user_command(char *user_command, size_t size);
-const char *find_command_path(const char *command);
-void execute_command(const char *user_command);
-void print_environment(void);
-void clear_command(void);
-void cd_command(const char *user_command);
+char *read_input(void);
+void free_last_command(void);
+size_t _strlen(const char *str);
+int execute(char *command);
+
+
+
 
 #endif

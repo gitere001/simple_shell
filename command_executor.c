8610217d3b02f798ca98126c73316c7c5b20533f
James gitere <sasmuypwa2019.com>
2023-11-15 12:15:04 -0800

    modifie

--- command_executor.c
+++ command_executor.c
@@ -1,88 +1,54 @@
 #include "sshell.h"
 
 /**
- * execute_command - Tokenize the user input, find the command path,
- * and execute it in a child process.
- * @user_command: The user-provided command to execute.
- * Return: void
+ * execute - Execute a command without arguments.
+ * @command: The command to be executed.
+ *
+ * Return: The exit status of the executed command.
  */
-
-void execute_command(const char *user_command)
+int execute(char *command)
 {
-	const char *command = user_command;
-	char *args[MAX_ARGS];
-	size_t num_args = 0;
-	pid_t child_id;
+	pid_t id;
+	int status = 0;
 
-	char *user_command_copy = strdup(user_command);
+	if (command == NULL)
+		return (status);
 
-	char *token = strtok(user_command_copy, " ");
-
-	while (token != NULL && num_args < MAX_ARGS - 1)
-	{
-		args[num_args++] = token;
-		token = strtok(NULL, " ");
-	}
-	args[num_args] = NULL;
-
-	if (num_args == 0)
-	{
-		free(user_command_copy);
-		return;
-	}
-	if (strcmp(args[0], "cd") == 0)
-	{
-		cd_command(user_command);
-		free(user_command_copy);
-		return;
-	}
-	if (strcmp(args[0], "clear") == 0)
-	{
-		setenv("TERM", "xterm-256color", 1);
-		clear_command();
-		free(user_command_copy);
-		return;
-	}
-
-	if (strchr(args[0], '/'))
-	{
-		command = args[0];
-	}
-	else
-	{
-		const char *full_path = find_command_path(args[0]);
-
-		if (full_path != NULL)
-		{
-			command = full_path;
-		}
-		else
-		{
-			ss_print("Command not found: ");
-			ss_print(args[0]);
-			ss_print("\n");
-			free(user_command_copy);
-			return;
-		}
-	}
-
-	child_id = fork();
-
-	if (child_id == -1)
+	id = fork();
+	if (id < 0)
 	{
 		perror("fork");
-		exit(EXIT_FAILURE);
+		return (1);
 	}
-	else if (child_id == 0)
+
+	if (id == 0)
 	{
-		execve(command, args, NULL);
-		perror("execve");
-		exit(EXIT_FAILURE);
+		char **args = malloc(2 * sizeof(char *));
+
+		if (args == NULL)
+		{
+			perror("malloc");
+			exit(EXIT_FAILURE);
+		}
+
+		args[0] = command;
+		args[1] = NULL;
+
+		if (execve(command, args, NULL) == -1)
+		{
+			perror("execve");
+			free(args);
+			exit(EXIT_FAILURE);
+		}
 	}
 	else
 	{
-		wait(NULL);
+		do
+
+		{
+			waitpid(id, &status, 0);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
 	}
 
-	free(user_command_copy);
+	return (status);
 }

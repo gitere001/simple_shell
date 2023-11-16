58f46411264d8bd155ec7c5ac7b4e049b30bb6e1
James gitere <sasmuypwa2019.com>
2023-11-16 13:02:27 +0300

    updated headerfile

--- sshell.h
+++ sshell.h
@@ -14,15 +14,39 @@
 #include <stdarg.h>
 #include <ctype.h>
 
-void ss_print(char *str);
-void _printerror(char *error);
+/*prompt_display.c*/
 void prompt_display(void);
+
+/*read_input.c*/
 char *read_input(void);
 void free_last_command(void);
-size_t _strlen(const char *str);
+
+/*command_executor.c*/
 int execute(char *command);
 
+/*ss_print*/
+void ss_print(char *str);
+void _printerror(char *error);
 
+/*string_operation1*/
+size_t _strlen(const char *str);
+int _strcmp(const char *str1, const char *str2);
+int _strncmp(const char *str1, const char *str2, size_t n);
+char *_strstr(char *haystack, char *needle);
+char *_strchr(char *str, char c);
 
+/*string_operations2*/
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, const char *src);
+char *_strdup(const char *str);
+int _putchar(char c);
+unsigned int _strspn(char *str, char *accept);
+
+/*strings_operations3*/
+int _atoi(const char *s);
+char *_memset(char *a, char bytes, unsigned int b);
+char *_memcpy(char *des, char *source, unsigned int bytes);
+void *_realloc(void *pt, unsigned int p_size, unsigned int n_size);
+void *_calloc(unsigned int nmemb, unsigned int size);
 
 #endif
